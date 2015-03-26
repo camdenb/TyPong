02d158cccd3a8bccabfab4b7a4a69c0ba392159d
camdenb <doctorshmoogle@gmail.com>
2015-03-25 21:12:50 -0700

    added learning!

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 require('words')
 vector = require('vector')
+Shake = require('shake')
 
 local tickCounter = 0
 local tickMax = 60
 
 local letters = {
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+}
+
+local errorsPerLetter = {
+	{'a', 1000}, {'b', 1000}, {'c', 1000}, {'d', 1000}, {'e', 1000}, {'f', 1000}, {'g', 1000}, {'h', 1000}, {'i', 1000}, {'j', 1000}, {'k', 1000}, {'l', 1000}, {'m', 1000}, {'n', 1000}, {'o', 1000}, {'p', 1000}, {'q', 1000}, {'r', 1000}, {'s', 1000}, {'t', 1000}, {'u', 1000}, {'v', 1000}, {'w', 1000}, {'x', 1000}, {'y', 1000}, {'z', 1000}
 }
 
 local answerWord = ''
@@ -21,6 +26,8 @@ local color_green = {0, 255, 0}
 local color_black = {0, 0, 0}
 local color_white = {255, 255, 255}
 local color_grey = {150, 150, 150}
+
+nextLetter = ''
 
 local wrong = false
 
@@ -42,16 +49,32 @@ xmult = 1
 
 local wordStartTime = 0
 
+local justHitCPU = false
+
 function love.load()
 
-	WINDOW_HEIGHT = 400
-	WINDOW_WIDTH = 400
+	WINDOW_HEIGHT = 500
+	WINDOW_WIDTH = 700
 
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {resizable=true, vsync=enableVsync, fsaa=0})
-	love.window.setTitle('Pong')
+	love.window.setTitle('TyPong')
 	love.graphics.setBackgroundColor(color_black)
 
+	if love.filesystem.exists('errorsPerLetter.txt') and true then
+		readErrors()
+	end
+
 	math.randomseed(os.time())
+
+	getScoreOfWord('asshole')
+
+	love.keyboard.setKeyRepeat( true )
+
+	love.audio.setVolume(.5)
+
+	Shake.reset()
+	Shake.amplitude = 0.1
+
 	-- answerWord = words[math.random(1, #words)]
 
 	sound_bounce_arr = {
@@ -89,7 +112,12 @@ function love.load()
 
 	sound_bounce_cpufail = love.audio.newSource('bounce3.mp3')
 
-	ball = {pos = vector(WINDOW_WIDTH / 2, 100), vel = vector(math.random(-200, 200) / 100, math.random(100, 200) / -100), size = 10}
+	ball = {pos = vector(WINDOW_WIDTH / 2, 100), vel = vector(0, math.random(100, 200) / -100), size = 10}
+	if math.random(0,1) == 0 then
+		ball.vel.x = math.random(-200, -100) / 100
+	else
+		ball.vel.x = math.random(100, 200) / 100
+	end
 	ball2 = {pos = ball.pos, vel = ball.vel, size = ball.size}
 	
 	largeFont = love.graphics.newFont(18)
@@ -108,6 +136,8 @@ function love.load()
 
 	wordCompleted = false
 
+	-- checkWord()
+
 end
 
 function love.resize(w, h)
@@ -116,6 +146,8 @@ function love.resize(w, h)
 end
 
 function love.update(dt)
+
+	Shake.update(dt)
 
 	tickCounter = tickCounter + 1
 	if tickCounter >= tickMax then
@@ -195,10 +227,11 @@ function love.update(dt)
 		cpuTargetX = ball2.pos.x - ball2.size / 2
 	end
 
+	--BOUNCE OFF OF PADDLE
 	if ball.pos.y > WINDOW_HEIGHT - 40 - PADDLE_HEIGHT and isBallBetweenPaddle(ball, vector(xPos, 0)) then
 		ball.pos.y = WINDOW_HEIGHT - 40 - PADDLE_HEIGHT
 		bounce()
-		xmult = math.random(75, 150) / (100 - mult * 10)
+		xmult = math.random(75, 125) / (100)
 		ball.vel = ball.vel * 1.2
 		ball.vel.x = ball.vel.x * xmult
 		ball.vel.y = -ball.vel.y
@@ -206,9 +239,10 @@ function love.update(dt)
 	end
 
 	--BOUNCE OFF OF TOP
-	if isBallBetweenPaddle(ball, cpu_paddle.pos) then
+	if isBallBetweenPaddle(ball, cpu_paddle.pos) and not justHitCPU then
 		if ball.pos.y < cpu_paddle.pos.y + PADDLE_HEIGHT then
 			bounce()
+			Shake.amplitude = 0.1
 			ball.pos.y = cpu_paddle.pos.y + PADDLE_HEIGHT
 			if ball.vel.y < 0 then
 				ball.vel.y = -ball.vel.y
@@ -221,6 +255,7 @@ function love.update(dt)
 	else
 		if ball.pos.y < 0 then
 			sound_bounce_cpufail:play()
+			justHitCPU = true
 			SCORE = SCORE + 1000
 			ball.pos.y = 0
 			if ball.vel.y < 0 then
@@ -258,6 +293,7 @@ function love.draw()
 	love.graphics.setColor(color_white)
 
 	love.graphics.print(math.floor(SCORE), WINDOW_WIDTH / 2, WINDOW_HEIGHT - 22)
+	-- love.graphics.print(nextLetter, WINDOW_WIDTH / 2, 200)
 
 	if gameIsOver then
 		love.graphics.print("FINAL SCORE", WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
@@ -269,9 +305,14 @@ function love.draw()
 	end
 	-- love.graphics.print(progress, 10, 200)
 	love.graphics.setFont(largeFont)
+
+	Shake.preDraw()
+
 	love.graphics.rectangle('fill', xPos, WINDOW_HEIGHT - 40, PADDLE_WIDTH, PADDLE_HEIGHT)
 	love.graphics.rectangle('fill', cpu_paddle.pos.x, cpu_paddle.pos.y, PADDLE_WIDTH, PADDLE_HEIGHT)
 	love.graphics.rectangle('fill', ball.pos.x, ball.pos.y, ball.size, ball.size)
+
+	Shake.postDraw()
 
 	if waitToStartCount > 0 then
 		love.graphics.print(waitToStartCount, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
@@ -299,16 +340,68 @@ function love.keypressed(key)
 	if not gameIsOver then
 		if key == 'backspace' then
 			currentWord = string.sub(currentWord, 1, #currentWord - 1)
-			checkWord()
+			if #currentWord > 0 then
+				checkWord()
+			end
 		elseif contains(letters, key) then
+			writeErrors()
 			sound_tw_arr[math.random(1, #sound_tw_arr)]:play()
 			currentWord = currentWord .. key
+			if nextLetter ~= nil and nextLetter ~= '' then
+				-- print(key, nextLetter)
+				if key == nextLetter then
+					letterCorrect(nextLetter)
+				else
+					letterIncorrect(nextLetter)
+				end
+			end
 			checkWord()
 			if currentWord == answerWord then
 				wordComplete()
 			end
 		end
 	end
+end
+
+function love.quit()
+	return false
+end
+
+function writeErrors()
+	love.filesystem.remove('errorsPerLetter.txt')
+	love.filesystem.write('errorsPerLetter.txt', '')
+	for i, v in pairs(errorsPerLetter) do
+		love.filesystem.append('errorsPerLetter.txt', v[2] .. '\n')
+	end
+end
+
+function readErrors()
+	local i = 1
+	local curLetter = letters[i]
+	for line in love.filesystem.lines("errorsPerLetter.txt") do
+
+		errorsPerLetter[i][2] = line
+
+		i = i + 1
+		curLetter = letters[i]
+	end
+end
+
+function getIndexByLetter(letter)
+	for i,v in ipairs(errorsPerLetter) do
+		if v[1] == letter then
+			return i 
+		end
+	end
+end
+
+function letterCorrect(letter)
+	errorsPerLetter[getIndexByLetter(letter)][2] = errorsPerLetter[getIndexByLetter(letter)][2] - 2
+end
+
+function letterIncorrect(letter)
+	-- print(letter)
+	errorsPerLetter[getIndexByLetter(letter)][2] = errorsPerLetter[getIndexByLetter(letter)][2] + 11
 end
 
 function wordComplete()
@@ -324,6 +417,7 @@ function wordComplete()
 			mult = 2
 		end
 		ball.vel = ball.vel * mult
+		Shake.amplitude = mult
 	end
 end
 
@@ -331,9 +425,14 @@ function newWord()
 	wordStartTime = love.timer.getTime()
 	ball.vel = ball.vel / mult
 	currentWord = ''
-	answerWord = words[math.random(1, #words)]
+	answerWord = selectNextWord()
 	wordCompleted = false
 	ratio = (newX - endXPos - PADDLE_WIDTH * .5) / (#answerWord)
+	checkNextLetter()
+end
+
+function checkNextLetter()
+	nextLetter = string.sub(answerWord, #currentWord + 1, #currentWord + 1)
 end
 
 function getNewX()
@@ -341,8 +440,11 @@ function getNewX()
 end
 
 function checkWord()
+	selectNextWord()
 	if string.sub(currentWord, 1, #currentWord) == string.sub(answerWord, 1, #currentWord) then
 		wrong = false
+		checkNextLetter()
+		-- print(nextLetter)
 		xPos = endXPos + #string.sub(currentWord, 1, #currentWord) * ratio
 		-- progress = (#string.sub(currentWord, 1, #currentWord) * ratio) / newX
 	else
@@ -365,6 +467,7 @@ function tick()
 	else
 		waitToStartCount = waitToStartCount - 1
 	end
+	justHitCPU = false
 end
 
 function start()
@@ -394,10 +497,51 @@ function setNewTargetAndRatio()
 	ratio = (newX - endXPos - PADDLE_WIDTH * .5) / (#answerWord)
 end
 
-function bounce()
+function bounce(shake)
 	sound_bounce_arr[math.random(1, #sound_bounce_arr)]:play()
+	if true then
+		Shake.more()
+	end
 end
 
 function gameOver()
 	gameIsOver = true
+end
+
+function getScoreOfWord(word)
+
+	local score = 0
+
+	for x = 1, string.len(word), 1 do
+		score = score + errorsPerLetter[getIndexByLetter(word:sub(x,x))][2]
+	end
+
+	return score
+
+
+end
+
+function selectNextWord()
+
+	local possibleWords = {}
+
+	for i = 1, 10, 1 do
+		possibleWords[i] = words[math.random(1, #words)]
+		-- print(possibleWords[i])
+	end
+
+	local highestScore = -100
+	local bestWord = possibleWords[1]
+
+	for i,v in ipairs(possibleWords) do
+		if getScoreOfWord(v) > highestScore then
+			highestScore = getScoreOfWord(v) - #v * 2
+			bestWord = v 
+		end
+	end
+
+	-- print('BEST WORD', bestWord)
+
+	return bestWord
+
 end
---
+++ shake.lua
@@ -0,0 +1,44 @@
+
+--[[
+
+creds to https://gist.github.com/MichaelCarius/7655420
+
+]]--
+
+local Shake = {}
+ 
+Shake.growth = 5
+Shake.amplitude = 1
+Shake.frequency = 100
+ 
+local amount
+local time
+ 
+function Shake.reset()
+    amount = 1
+    time = 0
+end
+ 
+function Shake.update(dt)
+    amount = math.max(1, amount ^ 0.9)
+    time = time + dt
+end
+ 
+function Shake.more()
+    amount = amount + Shake.growth
+end
+ 
+function Shake.preDraw()
+    local shakeFactor = Shake.amplitude * math.log(amount)
+    local waveX = math.sin(time * Shake.frequency)
+    local waveY = math.cos(time * Shake.frequency)
+ 
+    love.graphics.push()
+    love.graphics.translate(shakeFactor * waveX, shakeFactor * waveY)
+end
+ 
+function Shake.postDraw()
+    love.graphics.pop()
+end
+ 
+return Shake
